3f417546d9346a7b53f19202e057f700cfe23b8b
Mintesinot-Tefera <mintesinotteferamt@gmail.com>
2024-03-01 15:57:02 -0800

    task39

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -7,10 +7,18 @@
  * Return: pointer to the root node of the created Binary Heap, or NULL on
  * failure
  */
-
+ 
 heap_t *array_to_heap(int *array, size_t size)
 {
-	printf("%d", *array);
-	printf("%ld", size);
-	return (NULL);
+	size_t i = 0;
+	bst_t *root = NULL;
+
+	if (!array)
+		return (NULL);
+	while (i < size)
+	{
+		heap_insert(&root, array[i]);
+		i++;
+	}
+	return (root);
 }
